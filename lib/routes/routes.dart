971ec3805343132c6310ba56a615037f50abd678
gmulonga <gadsonmulonga@gmail.com>
2025-05-12 16:18:45 +0300

    feat: adding the app routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:pretium/views/homepage.dart';
+import 'package:pretium/views/auth/login_screen.dart';
+import 'package:pretium/views/get_started.dart';
 
 class AppRoutes {
   static const String home = '/';
+  static const String login = '/login';
+  static const String getStarted = '/getStarted';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
         return MaterialPageRoute(builder: (_) => Homepage());
+      case login:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case getStarted:
+        return MaterialPageRoute(builder: (_) => Home());
 
       default:
         return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text('Page not found')),
-          ),
+          builder:
+              (_) =>
+                  const Scaffold(body: Center(child: Text('Page not found'))),
         );
     }
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kWhite = Color(0xFFFFFFFF);
 const kDarkGreen = Color(0xFF15635e);
+const kLightGreen = Color(0xFFe7f0f0);
